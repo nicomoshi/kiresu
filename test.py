b81adc84f151af8b67d1270cdf0f9c7f2b522b29
rodolfoarguello <rodolfo@maskot.co>
2021-05-01 20:51:55 +1000

    - Working Kiresu! Implementation

--- test.py
+++ test.py
@@ -4,40 +4,98 @@ import pyrebase
 import requests
 import numpy as np
 from sklearn.metrics import pairwise
+from keras.models import load_model
+import firebase_admin
+from firebase_admin import credentials, firestore
 
-# Firebase config
-config = {
-  "apiKey": "AIzaSyCL6_tL1MRh4cdwk7QjlogFvlppvIsTM1E",
-  "authDomain": "kiresu-6d765.firebaseapp.com",
-  "databaseURL": "https://kiresu-6d765-default-rtdb.firebaseio.com",
-  "storageBucket": "kiresu-6d765.appspot.com"
+cred = credentials.Certificate("firebase-cred.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+
+def predict_rgb_image_vgg(image):
+    image = np.array(image, dtype='float32')
+    image /= 255
+    pred_array = model.predict(image)
+    result = gesture_names[np.argmax(pred_array)]
+    score = float("%0.2f" % (max(pred_array[0]) * 100))
+    return result, score
+
+def remove_background(frame):
+    fgmask = bgModel.apply(frame, learningRate=learningRate)
+    kernel = np.ones((3, 3), np.uint8)
+    fgmask = cv2.erode(fgmask, kernel, iterations=1)
+    res = cv2.bitwise_and(frame, frame, mask=fgmask)
+    return res
+
+# General  Hand Recognition Settings
+prediction = ''
+action = ''
+score = 0
+img_counter = 500
+
+# Gestures
+gesture_names = {0: 'Fist',
+                 1: 'L',
+                 2: 'Okay',
+                 3: 'Palm',
+                 4: 'Peace'}
+
+gesture_emojis = {
+    'Fist': '✊',
+    'L': '👆',
+    'Okay': '👌',
+    'Palm': '🤚',
+    'Peace': '✌️'
 }
 
-# Firebase connection
-firebase = pyrebase.initialize_app(config)
+# Hand Recognition Model
+model = load_model('models/VGG_cross_validated.h5')
 
-# Firebase database instance
-db = firebase.database()
+# parameters
+cap_region_x_begin = 0.75  # start point/total width
+cap_region_y_end = 0.6  # start point/total width
+threshold = 1  # binary threshold
+blurValue = 41  # GaussianBlur parameter
+bgSubThreshold = 100
+learningRate = 0
 
-# Get one name and image
-name = db.child("users").child("Rodolfo").get().val().get('name')
-image = db.child("users").child("Rodolfo").get().val().get('image')
+# variableslt
+isBgCaptured = 0  # bool, whether the background captured
+triggerSwitch = False  # if true, keyboard simulator works
 
-# Download image
-response = requests.get(image)
-r = open("Rodolfo.png", "wb")
-r.write(response.content)
-r.close()
+# users collection
+snapshots = list(db.collection(u'users').get())
+
+# number of users
+number_of_images = 0
+for snapshot in snapshots:
+    number_of_images += 1
+
+images = [] * (number_of_images+1)  # array for image storing
+encodings = [] * (number_of_images+1)  # array for image encodings
+
+known_face_names = []  # known faces
+known_gestures = []
+
+snapshot_controller = 0
+
 
 # Start camera
 video_capture = cv2.VideoCapture(0)
 
-images = []
-images.append(face_recognition.load_image_file('Rodolfo.png'))
-encodings = []
-encodings.append(face_recognition.face_encodings(images[0])[0])
-known_face_names = ['Rodolfo']
-
+for snapshot in snapshots:
+    get_name = snapshot.to_dict()["fName"]
+    response = requests.get(snapshot.to_dict()["photo_url"])
+    r = open(get_name + ".png", "wb")
+    r.write(response.content)
+    r.close()
+    images.append(face_recognition.load_image_file(get_name + ".png"))
+    encodings.append(face_recognition.face_encodings(images[snapshot_controller])[0])  #Fix
+    known_face_names.append(get_name)
+    known_gestures.append(snapshot.to_dict()["gesture"])
+    snapshot_controller += 1
 
 # Initialize some variables
 face_locations = []
@@ -45,22 +103,48 @@ face_encodings = []
 face_names = []
 process_this_frame = True
 
-i = 0
-_, frame=video_capture.read()
-back = None
-roi = cv2.selectROI(frame)
-(x, y, w, h) = tuple(map(int, roi))
+gesture_timer = 10
 
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
 
+    frame = cv2.bilateralFilter(frame, 5, 50, 100)  # smoothing filter
+    frame = cv2.flip(frame, 1)  # flip the frame horizontally
     # Resize frame of video to 1/4 size for faster face recognition processing
     small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_small_frame = small_frame[:, :, ::-1]
+    cv2.rectangle(frame, (int(cap_region_x_begin * frame.shape[1]), 0),
+                  (frame.shape[1], int(cap_region_y_end * frame.shape[0])), (255, 0, 0), 2)
 
+    bgModel = cv2.createBackgroundSubtractorMOG2(0, bgSubThreshold)
+
+    img = remove_background(frame)
+    img = img[0:int(cap_region_y_end * frame.shape[0]),
+          int(cap_region_x_begin * frame.shape[1]):frame.shape[1]]  # clip the ROI
+
+    # convert the image into binary image
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0)
+    capture = 0
+
+    # cv2.imshow('blur', blur)
+    ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    # Add prediction and action text to thresholded image
+    # cv2.putText(thresh, f"Prediction: {prediction} ({score}%)", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
+    # cv2.putText(thresh, f"Action: {action}", (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))  # Draw the text
+    # Draw the text
+    cv2.imshow('ori', thresh)
+    cv2.putText(frame, f"{prediction} ({score}%)", (1000, 400), cv2.FONT_HERSHEY_SIMPLEX, 1,
+                (0, 0, 0))
+
+    # copies 1 channel BW image to all 3 RGB channels
+    target = np.stack((thresh,) * 3, axis=-1)
+    target = cv2.resize(target, (224, 224))
+    target = target.reshape(1, 224, 224, 3)
+    prediction, score = predict_rgb_image_vgg(target)
     # Only process every other frame of video to save time
     if process_this_frame:
         # Find all the faces and face encodings in the current frame of video
@@ -68,6 +152,7 @@ while True:
         face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
         face_names = []
+        gestures = []
         for face_encoding in face_encodings:
             # See if the face is a match for the known face(s)
             matches = face_recognition.compare_faces(encodings, face_encoding)
@@ -77,14 +162,16 @@ while True:
             if True in matches:
                 first_match_index = matches.index(True)
                 name = known_face_names[first_match_index]
+                gesture = known_gestures[first_match_index]
 
             face_names.append(name)
+            gestures.append(gesture)
 
     process_this_frame = not process_this_frame
 
 
     # Display the results
-    for (top, right, bottom, left), name in zip(face_locations, face_names):
+    for (top, right, bottom, left), name, gesture in zip(face_locations, face_names, gestures):
         # Scale back up face locations since the frame we detected in was scaled to 1/4 size
         top *= 4
         right *= 4
@@ -99,63 +186,15 @@ while True:
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
 
-    if i < 60:
-        i += 1
-        if back is None:
-            back = frame[y:y + h, x:x + w].copy()
-            back = np.float32(back)
-        else:
+        if gesture_emojis[prediction] == gesture:
+            if score > 95:
+                gesture_timer -= 1
+                if gesture_timer == 0:
+                    print('door open')
+            else:
+                gesture_timer = 10
 
-            cv2.accumulateWeighted(frame[y:y + h, x:x + w].copy(), back, 0.2)
-    else:
-        # print(back.shape,frame.shape)
-        back = cv2.convertScaleAbs(back)
-        back_gray = cv2.cvtColor(back, cv2.COLOR_BGR2GRAY)
-        frame_gray = cv2.cvtColor(frame[y:y + h, x:x + w], cv2.COLOR_BGR2GRAY)
-
-        img = cv2.absdiff(back_gray, frame_gray)
-
-        ret, img = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        con, hie = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        img2 = img.copy()
-
-        con = max(con, key=cv2.contourArea)
-        conv_hull = cv2.convexHull(con)
-        cv2.drawContours(img, [conv_hull], -1, 225, 3)
-
-        top = tuple(conv_hull[conv_hull[:, :, 1].argmin()][0])
-        bottom = tuple(conv_hull[conv_hull[:, :, 1].argmax()][0])
-        left = tuple(conv_hull[conv_hull[:, :, 0].argmin()][0])
-        right = tuple(conv_hull[conv_hull[:, :, 0].argmax()][0])
-        cx = (left[0] + right[0]) // 2
-        cy = (top[1] + bottom[1]) // 2
-
-        dist = pairwise.euclidean_distances([left, right, bottom, top], [[cx, cy]])[0]
-        radi = int(0.80 * dist)
-
-        circular_roi = np.zeros_like(img, dtype='uint8')
-        cv2.circle(circular_roi, (cx, cy), radi, 255, 8)
-        wighted = cv2.addWeighted(img.copy(), 0.6, circular_roi, 0.4, 2)
-
-        mask = cv2.bitwise_and(img2, img2, mask=circular_roi)
-        # mask
-        con, hie = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-        count = 0
-        circumfrence = 2 * np.pi * radi
-        for cnt in con:
-            (m_x, m_y, m_w, m_h) = cv2.boundingRect(cnt)
-            out_wrist_range = (cy + (cy * 0.25)) > (m_y + m_h)
-            limit_pts = (circumfrence * 0.25) > cnt.shape[0]
-            if limit_pts and out_wrist_range:
-                # print(limit_pts,out_wrist_range)
-                count += 1
-
-        cv2.putText(frame, 'count: ' + str(count), (460, 70), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 250, 0), thickness=4)
-        cv2.rectangle(frame, (x, y), (x + w, y + h), 255, 3)
-        cv2.imshow('mask', mask)
-        cv2.imshow('frame', frame)
-        cv2.imshow('weight', wighted)
-
+    cv2.imshow('original', frame)
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -164,3 +203,4 @@ while True:
 # Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
+
